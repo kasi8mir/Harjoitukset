d5c541ce208c9ebeefca92a6e784e5bd9abe6487
kasimir leino <kasimirleino@hotmail.fi>
2025-03-28 13:06:01 +0200

    Add files via upload
    vk1 harjotteita. (ihan pihalla)

---
+++ vk1/script.js
@@ -0,0 +1,12 @@
+function funktio () {
+let p = 5 * 6;    
+let x, y, z;   
+x = 5; 
+y = 2; 
+z = x + y;
+
+console.log("This is z:", z);
+let toinen = "Tänne tulee arvo";
+console.log("Javascript-testi:", toinen);
+}
+
